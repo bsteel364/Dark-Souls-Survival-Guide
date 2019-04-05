eed96c5fba27fea21191ba693ecd217cef03fc87
Brandon Steel <bsteel364@gmail.com>
2019-04-05 18:42:24 -0400

    added tabview homescreen. inexplicable null ptr exception when adding to a list...

--- Dark Souls Survival Guide/src/application/Controller.java
+++ Dark Souls Survival Guide/src/application/Controller.java
@@ -6,14 +6,19 @@
 
 package application;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -56,6 +61,7 @@ public class Controller implements Initializable {
     	Main.stage.setScene(scene);
     	
     	Main.stage.show();
+    	fillLocationList();
     }
     
     public static String getUsername() {
@@ -69,14 +75,35 @@ public class Controller implements Initializable {
     
     
     ////////////////////////////////////////////////////////////////////
-    //			  Control Variables/Methods for HomeScreen.fxml	          //
+    //			  Control Variables/Methods for HomeScreen.fxml	      //
     ////////////////////////////////////////////////////////////////////
     @FXML public MenuItem quit;
-	@FXML public Button submitButton;
+	
 	
    public void quitItemClicked() {
 	   Main.stage.close();
    }
+   
+   
+   /********************************************************************\
+    		  Control Variables/Methods for Content Explorer
+   \********************************************************************/
+   
+   
+   @FXML public ListView<String> location_list;
+   
+   public void fillLocationList() {
+	   location_list.getItems().add("Hello");
+	   
+   }
+   
+   
+   
+   
+   
+   
+   
+   
     
    
 
